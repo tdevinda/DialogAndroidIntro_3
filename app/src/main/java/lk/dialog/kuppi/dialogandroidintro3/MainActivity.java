36829e3e2cca1ef422f98d81c3df22888d7be6c3
Tharaka Devinda <tharaka@vista.aero>
2015-05-08 02:05:49 +0530

    Handler - Uses a Handler to pass messages with threads

--- app/src/main/java/lk/dialog/kuppi/dialogandroidintro3/MainActivity.java
+++ app/src/main/java/lk/dialog/kuppi/dialogandroidintro3/MainActivity.java
@@ -1,5 +1,7 @@
 package lk.dialog.kuppi.dialogandroidintro3;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,18 +13,45 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static String EXTRA_RESULT = "lk.dialog.kuppi.dialogandroidintro3.result";
+    private static Handler messageHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView statusView = (TextView) findViewById(R.id.text_status);
+        final
         Button taskButton = (Button) findViewById(R.id.btn_doTask);
+
+        messageHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                TextView statusView = (TextView) findViewById(R.id.text_status);
+                int result = msg.getData().getInt(EXTRA_RESULT);
+                statusView.setText("Got " + result);
+
+            }
+        };
 
         taskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                statusView.setText("I clicked that button");
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int resultFromExec = Utilities.doLargeTask(5);
+                        Bundle data = new Bundle();
+                        data.putInt(EXTRA_RESULT, resultFromExec);
+
+                        Message msg = new Message();
+                        msg.setData(data);
+
+                        messageHandler.sendMessage(msg);
+                    }
+                }).start();
             }
         });
     }
@@ -44,5 +73,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    class DataHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+
+        }
     }
 }
